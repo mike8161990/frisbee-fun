2accaef72fe4acad07a739e0f706d258b7a3d5ea
Mike Schwartz <mike8161990@gmail.com>
2023-01-21 18:19:14 -0800

    Auto refresh catch events

--- src/server/home-base/src/app/catch-events/catch-events.component.ts
+++ src/server/home-base/src/app/catch-events/catch-events.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { Subscription, switchMap, timer } from 'rxjs';
 import { CatchEvent } from '../dtos';
 import { TelemetryService } from '../telemetry.service';
 
@@ -8,14 +9,23 @@ import { TelemetryService } from '../telemetry.service';
   templateUrl: './catch-events.component.html',
   styleUrls: ['./catch-events.component.scss']
 })
-export class CatchEventsComponent implements OnInit {
+export class CatchEventsComponent implements OnInit, OnDestroy {
   displayedColumns = ['timestamp', 'telemetryPoints'];
   dataSource = new MatTableDataSource<CatchEvent>();
+
+  subscription!: Subscription;
 
   constructor(private telemetry: TelemetryService) { }
 
   ngOnInit(): void {
-    this.telemetry.getCatchEvents()
+    this.subscription = timer(0, 1000)
+      .pipe(
+        switchMap(() => this.telemetry.getCatchEvents())
+      )
       .subscribe(events => this.dataSource.data = events);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
